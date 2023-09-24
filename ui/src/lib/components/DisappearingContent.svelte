9d420be6fc04a4e761786276e8a529c8f8c33107
Rachel Fish <itsrachelfish@gmail.com>
2023-09-24 00:13:54 -0600

    Fixed disappearing content in chrome 🙄

--- ui/src/lib/components/DisappearingContent.svelte
+++ ui/src/lib/components/DisappearingContent.svelte
@@ -1,11 +1,20 @@
 <script>
     let content;
 
+    // TODO: For best performance only change styles on the content when it is visible on the page?
     function handleScroll() {
-        // TODO: For best performance only change styles on the content when it is visible on the page?
-
         const boundingBox = content.getBoundingClientRect();
+
         content.style['mask-image'] =
+            `linear-gradient(
+                0deg,
+                rgba(0, 0, 0, 1),
+                rgba(0, 0, 0, 1) ${((boundingBox.height + boundingBox.top) - 244)}px,
+                transparent ${(boundingBox.height + boundingBox.top) - 122}px
+            )`;
+
+        // It's 2023 and mask image has been around for over 10 years... why is this still prefixed in chrome? 🙄
+        content.style['-webkit-mask-image'] =
             `linear-gradient(
                 0deg,
                 rgba(0, 0, 0, 1),
