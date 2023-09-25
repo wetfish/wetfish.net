5bc7f5d1fd068d1161be064b78e3d91f228ea0e9
Rachel Fish <itsrachelfish@gmail.com>
2023-09-24 22:51:05 -0600

    Fixed wetfish logo and updated menu styles

--- ui/src/lib/components/TopNav.svelte
+++ ui/src/lib/components/TopNav.svelte
@@ -23,9 +23,16 @@
         .menu {
             font-size: 24pt;
             border: 2px solid #0EE5A3;
-            color: #0EE5A3;
+            border-radius: 2px;
+            cursor: pointer;
+            background-color: RGBA(46, 207, 158, 0.25);
+            color: #fff;
             height: 1em;
             padding: 0.1em 0.25em 0.3em 0.25em;
+
+            &:hover {
+                background-color: transparent;
+            }
         }
     }
 
