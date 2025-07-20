b1d605e61bc1ba4e63c4553369c0a0944a61da5d
kristenwardphl@gmail.com <kristenwardphl@gmail.com>
2025-07-20 19:09:07 -0400

    14: wip - add background and bubbles to blobs

--- ui/src/lib/components/Blob.svelte
+++ ui/src/lib/components/Blob.svelte
@@ -38,12 +38,62 @@
     .blob-10 {
         clip-path: url(#blob-10);
     }
+    .blob-container {
+        position: relative;
+        background-repeat: no-repeat;
+        background-size: 100% 100%;
+    }
+    .blob-image,
+    .bubbles-image {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+    }
+    .blob-image {
+        z-index: -1;
+    }
+    .bubbles-image {
+        z-index: 2;
+        width: 100%;
+        height: 100%;
+    }
 </style>
 
 <script>
-    let { type, children } = $props();
+    let { type, children, color = 'blue' } = $props();
+    // let blobBackgroundURL = `$lib/images/blob${type}-${color}.svg`;
+    // import blobBackground from blobBackgroundURL;
+    // const images = import.meta.glob(
+    // '$lib/images/blobs/*.svg',
+    // { eager: true, import: 'default' },
+// );
+
+
+
 </script>
 
-<div class={`blob-${type}`}>
-    {@render children?.()}
+{type}
+{#await import(`$lib/images/blobs/blob${type}-${color}.svg`) then { default: bgSrc }}
+<!-- <div class="blob-container"> -->
+    <div class="blob-container" style="background-image: url('{bgSrc}');">
+    <!-- {#await import(`$lib/images/blobs/blob${type}-${color}.svg`) then { default: src }}
+        <img class="blob-image" {src}/>
+    {/await}        -->
+    <div class={`blob-${type}`}>
+        {@render children?.()}
+    </div>
+    {#await import(`$lib/images/blobs/bubbles${type}-${color}.svg`) then { default: src }}
+    <!-- <div class="bubble-container" style="width:100%; height: 100%; position: absolute; top: 0; left: 0; background-image: url('{src}'); background-size: 100% 100%; background-repeat: no-repeat;"> -->
+        <img class="bubbles-image" {src}/>
+    <!-- </div> -->
+    {/await}
 </div>
+{:catch error}
+    <div class={`blob-${type}`}>
+        {@render children?.()}
+    </div>
+    {#await import(`$lib/images/blobs/bubbles${type}-${color}.svg`) then { default: src }}
+        <img class="bubbles-image" {src}/>
+    {/await}
+{/await}
