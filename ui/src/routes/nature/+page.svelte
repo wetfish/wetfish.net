0fcc5db845d053a28b415baf0994a50de7c9bc26
Rachel Fish <itsrachelfish@gmail.com>
2026-01-02 17:29:07 -0700

    Wrote actual content for the nature page

--- ui/src/routes/nature/+page.svelte
+++ ui/src/routes/nature/+page.svelte
@@ -37,6 +37,10 @@
         }
     }
 
+    strong {
+        text-decoration: underline;
+    }
+
     .content {
         display: flex;
         justify-content: flex-start;
@@ -58,7 +62,7 @@
         }
 
         &.two {
-            padding-top: 0;
+            padding-top: 200px;
         }
 
         &.three {
@@ -114,7 +118,7 @@
             }
 
             &.three {
-                padding-top: 600px;
+                padding-top: 300px;
                 padding-bottom: 0;
             }
         }
@@ -129,8 +133,8 @@
 
     const accordionOptions = [
         {id: 'leaves', title: 'Collecting Leaves'},
-        {id: 'water', title: 'Watershed Management'},
-        {id: 'erosion', title: 'Erosion Control'},
+        {id: 'water', title: 'Wetland Restoration'},
+        {id: 'fire', title: 'Wildfire Mitigation'},
         {id: 'permaculture', title: 'Permaculture'},
     ];
 </script>
@@ -186,40 +190,68 @@
                 <Accordion options={accordionOptions}>
                     {#snippet leaves()}
                         <p>
-                            Every year our volunteers go out into their neighborhoods to collect leaves from people who would otherwise be throwing them away in the dump.
+                            Every year our volunteers go out into local neighborhoods to collect leaves from people who would otherwise be throwing them away in the dump.
                             These leaves are the perfect mulch and compost material for protecting young trees and creating water catchment structures.
                         </p>
 
                         <p>
                             Since 2023 we've collected an average of 1,000 bags of leaves every autumn, equating to approximately 10 tons of carbon sequestered every year from leaves alone.
+                        </p>
+
+                        <p>
                             We've used these leaves along with logs from the wildfire to create hugelkultur terraces along 2 acres of burn scar so far, with plans to continue expanding our restoration efforts every year.
                         </p>
                     {/snippet}
 
                     {#snippet water()}
                         <p>
-                            By building water catchment structures out of dead trees in wildfire burn scars, we slow down the water and give it time to absorb into the ground.
-                            This captured ground water replenishes perched aquifers and brings new life to old springs.
+                            We build water catchment structures out of dead trees in wildfire burn scars, slowing down the water and giving it time to absorb into the ground.
+                            This captured ground water recharges high-elevation aquifers and brings new life to old springs.
+                        </p>
+
+                        <p>
+                            A common technique for this type of work is called Low-Tech Process-Based Restoration (or PBR for short).
+                            These techniques include beaver bio-mimicry, using locally available materials like logs, mud, and rocks to create Beaver Dam Analogs (BDAs).
+                        </p>
+
+                        <p>
+                            By specifically targeting mountain streams we store water in the ground longer, reducing drought and flood risk downstream in the valleys where most cities and towns are located.
                         </p>
                     {/snippet}
 
-                    {#snippet erosion()}
+                    {#snippet fire()}
                         <p>
                             Wildfires create unstable landscapes prone to flash flooding and erosion.
+                            Water catchment structures reduce flooding while catching sediment and debris.
+                            This is a benefit for downstream water users by providing naturally filtered ground water.
                         </p>
 
                         <p>
-                            Water catchment structures reduce flooding while catching sediment and debris.
-                            This is a benefit for downstream water users by providing naturally filtered ground water.
+                            By cutting dead tree limbs and downed trees in the aftermath of wildfires, we gain valuable resources.
+                            We've built hiking trails, hugelkultur terraces, and beaver dams while simultaneously reducing fire risk.
+                        </p>
+
+                        <p>
+                            We reduce fuel loads, put carbon directly back into the soil by buring logs, and improve water retention to increase the growth speed and viability of the trees we plant.
                         </p>
                     {/snippet}
 
                     {#snippet permaculture()}
                         <p>
                             It comes in many names: permaculture, regenerative agriculture, agroforestry; but the principles are the same.
-                            First, observe the natural landscape, existing conditions, and study what was there historically.
-                            Then use locally available materials to create diverse microclimates that foster life.
-                            Now we are growing a food forest that provides abundance for humans as well as all other life in the surrounding ecosystem.
+                            <strong>First:</strong> Observe the natural landscape, existing conditions, and study the land's history.
+                            <strong>Then:</strong> Use locally available materials to create diverse microclimates that foster life.
+                        </p>
+
+                        <p>
+                            Since 2020, volunteers from Wetfish have been working to transform wildfire burn scars back into productive forests.
+                            The goal is to create permaculture food forests which provide food for people as well as a restored habitat for all the plants and animals that are native to the area.
+                        </p>
+
+                        <p>
+                            In 2025 we started a new experiment - using dead trees to build raised beds for growing annual food crops.
+                            These raised beds are built on the downhill slope side of the hugelkultur terraces we made over the past few years.
+                            This allows the uphill side of the terraces to be left undisturbed, perfect for growing native trees, shrubs, and perennial crops.
                         </p>
                     {/snippet}
                 </Accordion>
